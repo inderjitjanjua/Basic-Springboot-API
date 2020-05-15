3ef733d206380e4a37b1b37f5a2e416862a98ea9
inderjitjanjua <inderjit.j@hotmail.com>
2020-05-16 11:45:08 +1200

    Changed java version to 8

---
+++ src/test/java/com/example/bank/testBank/TestBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bank.testBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
